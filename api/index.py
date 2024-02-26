2d16a07589652020fd91b0d89a8abf974d638c2e
Nakaniss <nakaniss0sute@gmail.com>
2024-02-26 21:50:09 +0900

    import test2

--- api/index.py
+++ api/index.py
@@ -1,39 +1,11 @@
 from fastapi import FastAPI
-
-# from fastapi.middleware.cors import CORSMiddleware
-
 import sys
-import logging
 
-logging.warning(f"sys.path:{sys.path}")
-try:
-    from api.chirashi_stock import getjson_chirashi
-except Exception as e:
-    logging.warning(f"error1:{e}")
-    try:
-        from chirashi_stock import getjson_chirashi
-    except Exception as e:
-        logging.warning(f"error2:{e}")
-        try:
-            from .chirashi_stock import getjson_chirashi
-        except Exception as e:
-            logging.warning(f"error3:{e}")
-            raise e
-
-from api.Models import PostRequest
-
+print(f"sys.path:{sys.path}")
+from .chirashi_stock import getjson_chirashi
+from .Models import PostRequest
 
 app = FastAPI()
-
-# # Next.jsでAPIを叩くための記述
-# origins = ["http://localhost:8000"]  # ここにNext.jsのURLを設定
-# app.add_middleware(
-#     CORSMiddleware,
-#     allow_origins=origins,
-#     allow_credentials=True,
-#     allow_methods=["*"],
-#     allow_headers=["*"],
-# )
 
 
 # TODO:requestにハッシュ化したTOKEN含ませたい
@@ -44,3 +16,15 @@ async def receive_data(postrequest: PostRequest) -> dict:
     response = getjson_chirashi(shopid)
 
     return {"response": response}
+
+
+# from fastapi.middleware.cors import CORSMiddleware
+# # Next.jsでAPIを叩くための記述
+# origins = ["http://localhost:8000"]  # ここにNext.jsのURLを設定
+# app.add_middleware(
+#     CORSMiddleware,
+#     allow_origins=origins,
+#     allow_credentials=True,
+#     allow_methods=["*"],
+#     allow_headers=["*"],
+# )
